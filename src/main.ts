32f346dccbc23522d3032ccc3e43b26375e312aa
Collins <collinsmboya45@gmail.com>
2026-03-06 13:26:48 +0300

    added a file

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://shopvista-frontend.onrender.com',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
